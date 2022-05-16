f7173443f36c7060110281a40cc0a388da0cf4c3
R-T-Weerasinghe <ravindutharuka2001@gmail.com>
2022-05-16 12:46:18 +0530

    Initial

--- Week_2/lecture_1/part_c.py
+++
@@ -1,7 +0,0 @@
-# can't link the replit URL in the "Submit your assignment" section (in the LMS) as it says "Empty files are not allowed" 
-# when trying to link replit files. Tried linking a github page, still the results were same
-
-fname = "Ravindu"
-schl = "Ananda College"
-print("Hello, I am", fname)
-print("I am from ", schl)
